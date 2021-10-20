eb1275f511c5963f0a0f34023d812166db148aa6
Marwan Mustafa Fikrat <marwan.mm@live.com>
2021-10-20 19:11:17 +1300

    feat: remap C-\ C-n to C-o to exit term mode

--- nvim/.config/nvim/lua/terminal.lua
+++ nvim/.config/nvim/lua/terminal.lua
@@ -17,6 +17,7 @@ return {
     bindings = function(map)
         map("n", "<F4>", "':ToggleTerm dir=' . getcwd() . '<cr>'", { expr = true })
         map("i", "<F4>", "<esc><cmd>ToggleTerm<cr>", {})
+        map("t", "<C-o>", [[<C-\><C-n>]], { silent = true })
 
         vim.cmd [[
     autocmd TermEnter term://*toggleterm#* tnoremap <silent><F4> <Cmd>ToggleTerm<CR>
